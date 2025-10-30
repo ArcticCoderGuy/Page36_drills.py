28e63d5ae7e21d18cb191460affa3b847a171dc9
Markus Kaprio <markus.kaprio@edunord.se>
2025-10-30 15:25:54 +0200

    making the list

--- p36_drills3-1.py
+++ p36_drills3-1.py
@@ -1,1 +1,7 @@
 # p36_drills3-1.py
+# Goal is to understand the basic things on creating a list, how to print, sort, add/delete and edit items in a list.
+
+names = ["Tesla", "BYD", "Nissan", "Chevrolet", "BMW"]
+
+print("Start", names)
+
